14304af78733e7a2628caf19fa510c4ad19e3e70
tsmahajcsiks <tsmahajcsiks@avaya.com>
2019-06-12 15:02:52 +0200

    excerpts

---
+++ Chapter_1.R
@@ -0,0 +1,17 @@
+# In classical programming, the paradigm of symbolic AI, 
+# humans input rules (a program) and data to be processed 
+# according to these rules, and out come answers 
+
+# With machine learning, humans input data as well as the 
+# answers expected from the data, and out come the rules. 
+# These rules can then be applied to new data to produce
+# original answers. 
+
+# The central problem in machine learning and deep learning 
+# is to meaningfully transform data: in other words, to learn 
+# useful representations of the input data at 
+# hand—representations that get us closer to the expected
+# output. 
+
+# Learning, in the context of machine learning, describes 
+# an automatic search process for better representations. 
